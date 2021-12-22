6d864bc65101283f67f24b1852736dc122cc530b
root <3626@holbertonschool.com>
2021-12-22 12:20:50 -0800

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,39 @@
 #include "sort.h"
 /**
- *
- *
- *
- *
- *
- *
+ *selection_sort - sort array in ascending order
+ *@array: array
+ *@size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
+	int ct;
+	int temp;
+	size_t a;
+	size_t b;
+	size_t c;
 
+	if (size < 2)
+	{
+		return;
+	}
+	for (a = 0; a < size; a++)
+	{
+		c = a;
+		ct = 0;
+		for (b = a + 1; b < size; b++)
+		{
+			if (array[c] > array[b])
+			{
+				c = b;
+				ct++;
+			}
+		}
+		temp = array[a];
+		array[a] = array[c];
+		array[c] = temp;
+		if (ct > 0)
+		{
+			print_array(array, size);
+		}
+	}
 }
