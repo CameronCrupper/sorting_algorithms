9bd2f8600bd17fcee8caf1653d78ca220b6a83e4
CameronCrupper <guitardude20795@yahoo.com>
2022-05-05 19:16:50 +0000

    test

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
